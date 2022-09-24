22dcdda8603238f7aef4bbc00cf3e2218bce7537
guest <gayane.karapetuni@gmail.com>
2022-09-24 16:15:31 +0400

    PrimitiveTapes+

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {byte b = 112;
+        short s = 12300;
+        int i = 90683280;
+        long l = 6666666666666666666L;
+        float f;
+        f = 11.8F;
+        double d;
+        d = 125.4;
+        char ch1 = 'Y';
+        char ch2 = 'A';
+        char ch3 = 55;
+        boolean B;
+        B = false;
+
+        System.out.println("b1 is:" + b);
+        System.out.println("s is:" + s);
+        System.out.println("i is:" + i);
+        System.out.println("l is:" + l);
+        System.out.println("f is:" + f);
+        System.out.println("fd is:" + d);
+        System.out.println("ch1 is:" + ch1);
+        System.out.println("ch2 is:" + ch2);
+        System.out.println("ch3 is:" + ch3);
+        System.out.println("b is:" + b);
+    }
+}
+
---
+++ src/Type.java
@@ -0,0 +1,44 @@
+public class Type {
+    public static void main(String[] args) {
+        int i1 = 5;
+        int i2 = 10;
+        int i3 = 100;
+        int i4 = 9770;
+        int i5 = 627287360;
+        int i6 = '/';
+        char ch1 = '\u0064';
+        char ch2 = '\ua432';
+        char ch3 = 0X14;
+        char ch4 = '\144';
+        byte a1 = 64,b1;
+        int i;
+        i = a1 << 2;
+        b1 = (byte) (a1 << 2);
+        boolean a2 = true;
+        boolean b2 = false;
+
+
+        System.out.println("i1 is " + i1);
+        System.out.println("i2 is " + i2);
+        System.out.println("i3 is " + i3);
+        System.out.println("i4 is " + i4);
+        System.out.println("i5 is " + i5);
+        System.out.println("i6 is " + i6);
+        System.out.println("i6 is " + i6);
+        System.out.println("ch1 is " + ch1);
+        System.out.println("ch2 is " + ch2);
+        System.out.println("ch3 is " + ch3);
+        System.out.println("ch4 is " + ch4);
+        System.out.println("Original value of a1:"+ a1);
+        System.out.println("i and b1: "+ i + "" + b1);
+        System.out.println();
+        System.out.println();
+        System.out.println("a2 = "+ a2);
+        System.out.println("b2 = "+ b2);
+
+
+
+
+
+    }
+}
