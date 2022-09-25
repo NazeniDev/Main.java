c242cba9b7c40930fb7f5d72d41737716f89021c
guest <gayane.karapetuni@gmail.com>
2022-09-25 19:54:52 +0400

    PrimitiveTapes+

---
+++ src/IntegerType.java
@@ -0,0 +1,23 @@
+public class IntegerType {
+    public static void main(String[] args) {
+        int a = 10, b = 11, c = 12, d = 13;
+        int max = a;
+        if (b > max) ;
+        max = b;
+        if (c > max) ;
+        max = c;
+        if (d > max) ;
+        max = d;
+        System.out.println("max is " + max);
+
+        //Triangle
+
+        int a1 = 10;
+        int b1 = 14;
+        int c1 = 20;
+
+        if (a1 + b1 > c1 && a1 + c1 > b1 && c1 + b1 > a1) ;
+
+        System.out.println("Triangle");
+    }
+}
