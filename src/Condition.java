ea593836c4d13a57ee32b6dfcdb8be68ca094651
guest <gayane.karapetuni@gmail.com>
2022-09-25 16:22:08 +0400

    PrimitiveTapes+

---
+++ src/Condition.java
@@ -0,0 +1,31 @@
+public class Condition {
+    public static void main(String[] args) {
+        // Days of the week
+        int day = 7;
+        switch (day) {
+            case 1:
+                System.out.println("The day is Monday");
+                break;
+            case 2:
+                System.out.println("The day is Tuesday");
+                break;
+            case 3:
+                System.out.println("The day is Wednesday");
+                break;
+            case 4:
+                System.out.println("The day is Thursday");
+            case 5:
+                System.out.println("The day is Friday");
+            case 6:
+                System.out.println("The day is weekend");
+            case 7:
+                System.out.println("The day is weekend");
+                break;
+            default:
+                System.out.println("default");
+
+
+
+        }
+    }
+}
